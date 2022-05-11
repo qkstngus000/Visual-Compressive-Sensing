dc788c154cfc82f4bbb2b6871ff1e91e1acbe053
qkstngus000 <qkstngus000@gmail.com>
2022-05-11 03:28:05 -0700

    Standardized function with searching for Optimal hyperparameter

---
+++ src/V1_Compress.py
@@ -0,0 +1,58 @@
+import os
+import os.path as path
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.linalg as la
+
+from src.structured_random_features.src.models.weights import V1_weights
+
+# Packages for fft and fitting data
+from scipy import fftpack as fft
+from sklearn.linear_model import Lasso
+
+# Package for importing image representation
+from PIL import Image, ImageOps
+
+# Package for storing reformed image
+import imageio
+
+def generateY(W, img):
+    n, m = img.shape
+    y = W @ img.reshape(n * m, 1)
+    return y
+
+def generateV1Variables(numCell, cellSize, sparseFreq, img):
+    # Get size of image
+    dim = np.asanyarray(img).shape
+    n, m = dim
+    # Store generated V1 cells in W
+    W = V1_weights(numCell, dim, cellSize, sparseFreq) 
+    
+    # Retrieve y from W @ imgArr
+    y = W @ img.reshape(n*m, 1)
+
+    # Resize W to shape (numCell, height of image, width of image) for fetching into function
+    W = W.reshape(numCell, dim[0], dim[1])
+    return W, y
+
+def compress(W, y, alpha):
+    sampleSz, n, m = W.shape
+    
+    ## WΨ
+    theta = fft.dctn(W.reshape(sampleSz, n, m), norm = 'ortho', axes = [1, 2])
+    theta = theta.reshape(sampleSz, n * m)
+
+    ## Initialize Lasso and Fit data
+    mini = Lasso(alpha = alpha)
+    mini.fit(theta, y)
+    
+    ## Retrieve sparse vector s
+    s = mini.coef_
+    
+    # Reform the image using sparse vector s with inverse descrete cosine
+    reform = fft.idctn(s.reshape(n, m), norm='ortho', axes=[0,1])
+    
+    #return theta, reformed img, sparse vectors
+    return theta, reform, s
