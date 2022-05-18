53811ad7bf3caf4c224c7ded0c36e347d72c7308
qkstngus000 <qkstngus000@gmail.com>
2022-05-18 15:31:22 -0700

    changed formatting

--- src/V1_Compress.py
+++ src/V1_Compress.py
@@ -6,31 +6,31 @@ from src.structured_random_features.src.models.weights import V1_weights
 from scipy import fftpack as fft
 from sklearn.linear_model import Lasso
 
-def generateY(W, img):
+def generate_Y(W, img):
     n, m = img.shape
     y = W @ img.reshape(n * m, 1)
     return y
 
-def generateV1Variables(numCell, cellSize, sparseFreq, img):
+def generate_V1_variables(num_cell, cell_size, sparse_freq, img):
     # Get size of image
     dim = np.asanyarray(img).shape
     n, m = dim
     # Store generated V1 cells in W
-    W = V1_weights(numCell, dim, cellSize, sparseFreq) 
+    W = V1_weights(num_cell, dim, cell_size, sparse_freq) 
     
     # Retrieve y from W @ imgArr
     y = W @ img.reshape(n*m, 1)
 
-    # Resize W to shape (numCell, height of image, width of image) for fetching into function
-    W = W.reshape(numCell, dim[0], dim[1])
+    # Resize W to shape (num_cell, height of image, width of image) for fetching into function
+    W = W.reshape(num_cell, dim[0], dim[1])
     return W, y
 
 def compress(W, y, alpha):
-    sampleSz, n, m = W.shape
+    sample_sz, n, m = W.shape
     
     ## WΨ
-    theta = fft.dctn(W.reshape(sampleSz, n, m), norm = 'ortho', axes = [1, 2])
-    theta = theta.reshape(sampleSz, n * m)
+    theta = fft.dctn(W.reshape(sample_sz, n, m), norm = 'ortho', axes = [1, 2])
+    theta = theta.reshape(sample_sz, n * m)
 
     ## Initialize Lasso and Fit data
     mini = Lasso(alpha = alpha)
